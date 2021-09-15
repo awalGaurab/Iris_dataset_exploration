08ef59efd85053d20926ebb5fee71d87503e480b
Gaurab Awal <79394101+awalGaurab@users.noreply.github.com>
2021-09-15 20:10:13 +0545

    Add comments to the python code.

--- explore.py
+++ explore.py
@@ -1,18 +1,25 @@
 import pandas as pd
 
 def stat_function():
+  '''This function take input from user and give output with respect to the input number.
+     You can select number 1 to read minimum value, number 2 to get maximum value, 
+     number 3 to get mean and mumber 4 for standard deviation.'''
+  
   print("Type 1 for minimum value of each column \nType 2 for maximum of each column \nType 3 for mean of each column \nType 4 for standard deviation of each column")
   print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+  
+  # read dataset
   df = pd.read_csv('iris.csv')
 
   try:
+    # take input from user
     input_value = int(input("Enter number to select: "))
     if input_value == 1:
         print("You have selected minimum value.")
-        col1 = round(df.iloc[:,0].min(),2)
-        col2 = round(df.iloc[:,1].min(),2)
-        col3 = round(df.iloc[:,2].min(),2)
-        col4 = round(df.iloc[:,3].min(),2)
+        col1 = round(df.iloc[:,0].min(),2) # use iloc to select all row and column 1 (sepal length) value. 
+        col2 = round(df.iloc[:,1].min(),2) # use iloc to select all row and column 2 (sepal width) value. 
+        col3 = round(df.iloc[:,2].min(),2) # use iloc to select all row and column 3 (petal length) value . 
+        col4 = round(df.iloc[:,3].min(),2) # use iloc to select all row and column 4 (petal width) value. 
         print("Minimum value of sepal length is {0},sepal width is {1},petal length is {2},petal width is {3}.".format(col1,col2,col3,col4))
 
     elif input_value == 2:
